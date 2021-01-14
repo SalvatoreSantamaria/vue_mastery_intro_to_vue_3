5f8cf602673a5a6e18192e343f6a3ddbf518d73a
Salvatore Santamaria <sam.santamaria@gmail.com>
2021-01-14 12:31:21 -0800

    Event Handling

--- vue_3/main.js
+++ vue_3/main.js
@@ -13,10 +13,23 @@ const app = Vue.createApp({
       inventory: 9, 
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 1, color: 'green' },
-        { id: 2, color: 'blue'  }
+        { id: 1, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2, color: 'blue', image: './assets/images/socks_blue.jpg'  }
       ],
-      sizes: ['Small', "Medium", "Large"]
+      sizes: ['Small', "Medium", "Large"],
+      cart: 0
+    }
+  }, 
+  methods: {
+    addToCart() {
+      this.cart += 1
+    }, 
+    removeFromCart() {
+      this.cart -= 1
+    },
+    updateImage(variantImage) {
+      // set the image to the input mouseover image
+      this.image = variantImage 
     }
   }
 })
